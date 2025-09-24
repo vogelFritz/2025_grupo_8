1218d24d3addd15ab5a5e6700961778a5360dca4
Francisco <franciscosantin2002@gmail.com>
2025-09-24 15:21:00 -0300

    ViajeTest empezado

---
+++ src/tpfinal/ViajeTest.java
@@ -0,0 +1,49 @@
+package tpfinal;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import modeloDatos.ChoferTemporario;
+import modeloDatos.Cliente;
+import modeloDatos.Moto;
+import modeloDatos.Pedido;
+import modeloDatos.Viaje;
+import util.Constantes;
+
+public class ViajeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testConstructorValido() {
+		final ChoferTemporario chofer = new ChoferTemporario("test","test");
+		final Cliente cliente = new Cliente("test","test","test");
+		final Pedido pedido = new Pedido(cliente,1,false,false,10,Constantes.ZONA_STANDARD);
+		final Moto moto = new Moto("test");
+		final Viaje viaje = new Viaje(pedido, chofer, moto);
+		assertSame(viaje.getCalificacion(), 0);
+		assertSame(viaje.getChofer(), chofer);
+		assertSame(viaje.getPedido(), pedido);
+		assertSame(viaje.getVehiculo(), moto);
+	}
+
+}
